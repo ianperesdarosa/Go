4043619351858a803068b5d29af12c9b27197ce3
ianperesdarosa <ianperesdarosa2004@gmail.com>
2023-10-11 17:29:22 -0300

    commit

--- helloword.go
+++ helloword.go
@@ -2,9 +2,9 @@ package main
 import ("fmt")
 
 func main() {
-	Saudacao("Ian")
+	soma(2, 2)
 }
 
-func Saudacao(nome string){
-	fmt.Println("Hello, " + nome + "!")
+func soma(x int, y int) {
+	fmt.Print(x + y)
 }
